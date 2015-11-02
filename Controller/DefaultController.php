cd2389be06cf75b4e6df6bfbeea1449ced83fc34
miguelflorido <miguel5fv@gmail.com>
2015-11-02 10:04:26 +0100

    Simple and sample api rest using TDD

--- Controller/DefaultController.php
+++ Controller/DefaultController.php
@@ -6,8 +6,30 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
 {
-    public function indexAction($name)
+    public function indexAction($name, Request $request)
     {
-        return $this->render('RestfulSampleBundle:Default:index.html.twig', array('name' => $name));
+        /**
+         * HTTP response codes
+         *  @see: https://github.com/symfony/http-foundation/blob/master/Response.php
+         */
+        $response       = new Response();
+        if (empty($name)) {
+            $response->setStatusCode(Response::HTTP_NOT_FOUND);
+        } else if( !$request->isMethod('GET')) {
+            $response->setStatusCode(Response::HTTP_NOT_IMPLEMENTED);
+        } else {
+            $apiResponse    = array(
+                'name'  => $name,
+                '_self'  => $this->generateUrl(
+                    'restful_sample_helloworld',
+                    array('name' => $name)
+                )
+            );
+            $response->setContent(json_encode($apiResponse));
+        }
+
+        $response->headers->set('Content-Type', 'application/json');
+
+        return $response;
     }
 }
--- Tests/Controller/DefaultControllerTest.php
+++ Tests/Controller/DefaultControllerTest.php
@@ -10,8 +10,50 @@ class DefaultControllerTest extends WebTestCase
     {
         $client = static::createClient();
 
-        $crawler = $client->request('GET', '/hello/Fabien');
+        $apiResponse    = $client->request('GET', '/hello/Fabien');
+        $jsonExpected   = json_encode(array(
+            'name'      => 'Fabian',
+            '_self'     => $apiResponse->getUri()
+        ));
+        $this->assertEquals(
+            Response::HTTP_CREATED,
+            $apiResponse->getResponse()->getStatusCode(),
+            'Expects to recieved a 200 ok status code'
+        );
+        $this->assertEquals(
+            $jsonExpected,
+            $apiResponse->getResponse()->getContent(),
+            'Expects the exact Json encoded array content'
+        );
+    }
 
-        $this->assertTrue($crawler->filter('html:contains("Hello Fabien")')->count() > 0);
+    public function testIndexWithoutParameters()
+    {
+        $client = static::createClient();
+
+        $apiResponse = $client->request('GET', '/hello/');
+
+        $this->assertEquals(
+            Response::HTTP_NOT_FOUND,
+            $apiResponse->getResponse()->getStatusCode(),
+            'Expects 404 not found status code'
+        );
+        $this->assertEmpty(
+            $apiResponse->getResponse()->getContent(),
+            'Expects empty content'
+        );
+    }
+
+    public function testIndexWithWrongMethod()
+    {
+        $client = static::createClient();
+
+        $apiResponse = $client->request('DELETE', '/hello/fabian');
+
+        $this->assertEquals(
+            Response::HTTP_NOT_IMPLEMENTED,
+            $apiResponse->getResponse()->getStatusCode(),
+            'Expects a 501 method not implemented status code because delete is not implemented');
+        $this->assertEmpty($apiResponse->getResponse()->getContent());
     }
 }
